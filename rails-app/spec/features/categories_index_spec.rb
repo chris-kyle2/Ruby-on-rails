117be0856f1aaf09b6cb7a4ac0f718014906d931
chris-kyle2 <22ms0010@iitism.ac.in>
2025-06-29 18:59:27 +0530

    Fixing bugs

--- rails-app/spec/features/categories_index_spec.rb
+++ rails-app/spec/features/categories_index_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Categories Page Features', type: :feature do
   before(:each) do
-    visit user_session_path
-    user1 = User.create!(
-      name: 'mike', email: 'mike@gmail.com', password: 'mike123'
+    # Create the user first
+    user = User.create!(
+      name: 'mike',
+      email: 'mike@gmail.com',
+      password: 'mike123'
     )
+
+    # Log in the user
+    visit user_session_path
     fill_in 'Email', with: 'mike@gmail.com'
     fill_in 'Password', with: 'mike123'
     click_button 'Log in'
 
-    category1 = user1.categories.new(name: 'Safari')
-    category1.image.attach(
+    # Create a category for the user
+    category = user.categories.new(name: 'Safari')
+    category.image.attach(
       io: File.open(Rails.root.join('spec', 'img', 'safari.png')),
       filename: 'safari.png',
-      content_type: 'application/png'
+      content_type: 'image/png'
     )
-    category1.save!
+    category.save!
+
+    # Navigate to the categories page
     visit categories_path
   end
 
@@ -29,15 +37,15 @@ RSpec.describe 'Categories Page Features', type: :feature do
       expect(page).to have_content 'Safari'
     end
 
-    it 'should see total amount of transactions' do
+    it 'the total amount of transactions' do
       expect(page).to have_content '$0.0'
     end
 
-    it 'should see a button to add new category when clicked' do
-      expect(page).to have_button('New category')
+    it 'a button to add a new category' do
+      expect(page).to have_button 'New category'
     end
 
-    it 'a link to new category' do
+    it 'a link to create a new category' do
       expect(page).to have_link 'New category'
     end
   end
