01c8da33267e1f70ceddd30e7d608d64b367c5d6
chris-kyle2 <22ms0010@iitism.ac.in>
2025-06-29 17:58:54 +0530

    testing steps

--- rails-app/spec/rails_helper.rb
+++ rails-app/spec/rails_helper.rb
@@ -9,6 +9,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'webdrivers'
+Webdrivers::Chromedriver.required_version = '138.0.7204.49'
 
 # Optional: Let webdrivers handle Chrome versions automatically.
 # If you encounter issues, you can lock to a specific version like this:
